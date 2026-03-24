3316d41f1f4b95faee0bb6ca9523ad7684300674
Pedro Reinbold <phreinbold@gmail.com>
2026-03-24 10:31:03 -0300

    Aula heranca 01

---
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/test/HerancaTest01.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.javacore.Hheranca.test;
+
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest01 {
+    static void main() {
+        Endereco endereco = new Endereco();
+        Endereco endereco2 = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-678");
+        endereco2.setRua("Rua 6");
+        endereco2.setCep("012345-000");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Pedro");
+        pessoa.setCpf("1234567890");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Lua");
+        funcionario.setCpf("1234567890");
+        funcionario.setEndereco(endereco2);
+        funcionario.setSalario(20000);
+        System.out.println("----------");
+        funcionario.imprime();
+    }
+}
