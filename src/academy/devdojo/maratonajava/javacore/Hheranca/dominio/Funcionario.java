60396bcbc51b236854a15d885b2aabf34971525b
Pedro Reinbold <phreinbold@gmail.com>
2026-03-24 10:52:33 -0300

    Aula heranca 03 Protected

--- src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/dominio/Funcionario.java
@@ -7,6 +7,10 @@ public class Funcionario extends Pessoa {
         System.out.println(this.salario);
     }
 
+    public void relatorioPagamento(){
+        System.out.println("Eu "+this.nome+"recebi o salario de "+this.salario);
+    }
+
     private double salario;
 
     public double getSalario() {
